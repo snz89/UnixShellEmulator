b3897edb2ef4ec03b6e1433fa82604bac8805d21
seanz <seanzfx@yandex.ru>
2024-09-29 14:20:10 +0300

    feat: implemented basic Emulator class

---
+++ emulator.py
@@ -0,0 +1,28 @@
+import tarfile
+import csv
+
+class Emulator:
+    def __init__(self, filesystem):
+        self.current_path = ""
+        self.filesystem = filesystem
+    
+    def __ls(self):
+        pass
+    
+    def __cd(self):
+        pass
+    
+    def __exit(self):
+        pass
+    
+    def __pwd(self):
+        pass
+    
+    def __history(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
