b18370d1bc416f98edc868f5226290a743d521c1
seanz <seanzfx@yandex.ru>
2024-10-06 13:43:29 +0300

    feat: Add command-line argument support

--- emulator.py
+++ emulator.py
@@ -2,29 +2,34 @@ import tarfile
 import csv
 import os
 import datetime
+import argparse
+
 
 class Emulator:
-    def __init__(self, username, computername, filesystem, log_file):
+    def __init__(self, username, computername, filesystem, log_file, script):
         self.current_path = "/"
         self.username = username
         self.computername = computername
         self.filesystem = tarfile.open(filesystem, "r")
         self.log_file = log_file
+        self.script_file = script
         self.history = []
-    
+
     def execute(self):
         while True:
-            command = input(f'{self.username}@{self.computername}:{self.current_path} ')
+            command = input(
+                f'{self.username}@{self.computername}:{self.current_path} ')
             self.history.append(command)
             self.__log_command(command)
             self.__execute_command(command)
-    
+
     def __execute_command(self, command):
         command_parts = command.split()
         command_name = command_parts[0]
-        
+
         if command_name == "ls":
-            path = command_parts[1] if len(command_parts) > 1 else self.current_path
+            path = command_parts[1] if len(
+                command_parts) > 1 else self.current_path
             self.__ls(path)
         elif command_name == "cd":
             try:
@@ -44,7 +49,7 @@ class Emulator:
 
     def __ls(self, path):
         file_list = self.filesystem.getnames()
-        
+
         if path == "/":
             result = []
             for item in file_list:
@@ -70,44 +75,52 @@ class Emulator:
                             result.append(parts[0] + "/")
             for item in sorted(result):
                 print(item)
-        
-        
-    
+
     def __cd(self, path):
         file_list = self.filesystem.getnames()
-        print(file_list)
-        
+
         if path == "/":
             self.current_path = "/"
             return
-        
+
         if path.startswith("/"):
             full_path = path
         else:
             full_path = os.path.join(self.current_path, path)
-            
+
         if full_path[1:] in file_list:
             self.current_path = full_path
         else:
             print(f"cd: can't cd to {path}: No such file or directory")
-        
-    
+
     def __pwd(self):
         print(self.current_path)
-    
+
     def __history(self):
         for i, command in enumerate(self.history):
             print(f"{i+1} {command}")
-    
+
     def __log_command(self, command):
         with open(self.log_file, "a", newline="") as f:
             writer = csv.writer(f)
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             writer.writerow([timestamp, self.username, command])
 
+
 def main():
-    em = Emulator("user", "computer", "test.tar", "log.csv")
-    em.execute()
+    parser = argparse.ArgumentParser(description="Unix Shell Emulator")
+    
+    parser.add_argument("-u", "--user", default="localhost", help="Username")
+    parser.add_argument("-c", "--computer", default="localhost", help="Username")
+    parser.add_argument("filename", help="Path to the filesystem tar archive")
+    parser.add_argument("-l", "--log", default="log.csv", help="Path to the logfile")
+    parser.add_argument("-s", "--script", help="Path to the start script")
+    
+    args = parser.parse_args()
+    
+    emulator = Emulator(args.user, args.computer, args.filename, args.log, args.script)
+    emulator.execute()
+
 
 if __name__ == "__main__":
     main()
