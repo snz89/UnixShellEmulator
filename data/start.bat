a2e0d784db79fd2cd8b962d86fbc7bafaa2b8233
seanz <seanzfx@yandex.ru>
2024-10-06 15:58:26 +0300

    feat: Organize project structure

---
+++ data/start.bat
@@ -0,0 +1,3 @@
+@echo off
+python ../src/emulator.py test.tar -u user -c mycomputer -l log.csv -s script.txt
+pause
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python emulator.py test.tar -u user -c mycomputer -l log.csv -s script.txt
-pause
