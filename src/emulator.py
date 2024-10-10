fff43407d97520abccfe296ddab049c58ee03c76
seanz <seanzfx@yandex.ru>
2024-10-10 20:21:42 +0300

    fix: ls now reports "No such file or directory" when file is missing

--- src/emulator.py
+++ src/emulator.py
@@ -55,7 +55,8 @@ class Emulator:
         if path.startswith("/"):
             full_path = path
         else:
-            full_path = os.path.join(self.current_path, path).replace("\\", "/")
+            full_path = os.path.join(
+                self.current_path, path).replace("\\", "/")
 
         if full_path == "/":
             result = set()
@@ -65,23 +66,29 @@ class Emulator:
                     result.add(item + ("/" if tarinfo.isdir() else ""))
                 else:
                     parts = item.split("/")
-                    result.add(parts[0] + ("/" if self.filesystem.getmember(parts[0]).isdir() else ""))
+                    result.add(
+                        parts[0] + ("/" if self.filesystem.getmember(parts[0]).isdir() else ""))
             for item in sorted(list(result)):
                 print(item)
         else:
-            prefix = full_path[1:] + "/"
-            result = set()
-            for item in file_list:
-                if item.startswith(prefix) and item != prefix:
-                    rest = item[len(prefix):]
-                    tarinfo = self.filesystem.getmember(item)
-                    if "/" not in rest:
-                        result.add(rest + ("/" if tarinfo.isdir() else ""))
-                    else:
-                        parts = rest.split("/")
-                        result.add(parts[0] + ("/" if self.filesystem.getmember(prefix + parts[0]).isdir() else ""))
-            for item in sorted(list(result)):
-                print(item)
+            try:
+                tarinfo = self.filesystem.getmember(full_path[1:])
+                prefix = full_path[1:] + "/"
+                result = set()
+                for item in file_list:
+                    if item.startswith(prefix) and item != prefix:
+                        rest = item[len(prefix):]
+                        tarinfo = self.filesystem.getmember(item)
+                        if "/" not in rest:
+                            result.add(rest + ("/" if tarinfo.isdir() else ""))
+                        else:
+                            parts = rest.split("/")
+                            result.add(
+                                parts[0] + ("/" if self.filesystem.getmember(prefix + parts[0]).isdir() else ""))
+                for item in sorted(list(result)):
+                    print(item)
+            except KeyError:
+                print(f"ls: {path}: No such file or directory")
 
     def cd(self, path):
         file_list = self.filesystem.getnames()
@@ -93,11 +100,12 @@ class Emulator:
         if path.startswith("/"):
             full_path = path
         else:
-            full_path = os.path.join(self.current_path, path).replace("\\", "/")
+            full_path = os.path.join(
+                self.current_path, path).replace("\\", "/")
 
         try:
             tarinfo = self.filesystem.getmember(full_path[1:])
-            
+
             if tarinfo.isdir():
                 self.current_path = full_path
             else:
@@ -117,33 +125,36 @@ class Emulator:
             writer = csv.writer(f)
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             writer.writerow([timestamp, self.username, command])
-    
+
     def run_script(self, script_file):
         try:
             with open(script_file, "r") as f:
                 for line in f:
                     command = line.strip()
-                    print(f'{self.username}@{self.computername}:{self.current_path} {command}')
+                    print(
+                        f'{self.username}@{self.computername}:{self.current_path} {command}')
                     self.history.append(command)
                     self.log_command(command)
                     self.execute_command(command)
         except FileNotFoundError:
             print(f"{script_file}: No such file or directory")
-                    
 
 
 def main():
     parser = argparse.ArgumentParser(description="Unix Shell Emulator")
-    
+
     parser.add_argument("-u", "--user", default="localhost", help="Username")
-    parser.add_argument("-c", "--computer", default="localhost", help="Username")
+    parser.add_argument("-c", "--computer",
+                        default="localhost", help="Username")
     parser.add_argument("filename", help="Path to the filesystem tar archive")
-    parser.add_argument("-l", "--log", default="log.csv", help="Path to the logfile")
+    parser.add_argument("-l", "--log", default="log.csv",
+                        help="Path to the logfile")
     parser.add_argument("-s", "--script", help="Path to the start script")
-    
+
     args = parser.parse_args()
-    
-    emulator = Emulator(args.user, args.computer, args.filename, args.log, args.script)
+
+    emulator = Emulator(args.user, args.computer,
+                        args.filename, args.log, args.script)
     emulator.execute()
 
 
